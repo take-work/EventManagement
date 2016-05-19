c3dde732bda3e27ef5827033937d2bdb2f51ca9e
take-work <ts.lworkspace@gmail.com>
2016-05-19 15:52:06 +0900

    イベント一覧のためのテーブルを作成した。(中身は空です。)(Issue #2)

---
+++ resources/views/eventList.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>イベント一覧</title>
+</head>
+<body>
+  <font size="4">イベント一覧</font><br><br>
+  <table width="1300" border="10" cellspacing="0" cellpadding="8" bordercolor="#ffd700">
+    <tbody>
+      <tr>
+        <th>開始日</th>
+        <th>終了日</th>
+        <th>イベント名</th>
+        <th>主催者</th>
+        <th>スタッフ数</th>
+        <th>サークル数</th>
+        <th>準備費用</th>
+        <th>合計売上</th>
+        <th>純利益</th>
+        <th>データの編集</th>
+        <th>データの削除</th>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>

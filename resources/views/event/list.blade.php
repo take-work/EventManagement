92731167f322609cc04c81b83907c95e3491cdfe
take-work <ts.lworkspace@gmail.com>
2016-05-20 15:42:44 +0900

    イベント一覧ページをテーブルにして見易くした。(Issue #2)

--- resources/views/event/list.blade.php
+++ resources/views/event/list.blade.php
@@ -4,13 +4,67 @@
   <h3>イベント一覧</h3>
 
   <hr/>
-    
-  @foreach($events as $event)
-    <article>
-        {{ $event->name }}
-      <div class="body">
-        {{ $event->host }}
-      </div>
-    </article>
-    @endforeach
+
+  <table width="1300" border="10" cellspacing="0" cellpadding="8" bordercolor="#ffd700">
+    <tbody>
+      <tr>
+        <th>開始日</th>
+        <th>終了日</th>
+        <th>イベント名</th>
+        <th>主催者</th>
+        <th>スタッフ数</th>
+        <th>サークル数</th>
+        <th>準備費用</th>
+        <th>合計売上</th>
+        <th>純利益</th>
+        <th>データの編集</th>
+        <th>データの削除</th>
+      </tr>
+
+      @foreach($events as $event)
+        <tr>
+          <td align="center">
+            {{ $event->startDay }}
+          </td>
+
+          <td align="center">
+            {{ $event->endDay }}
+          </td>
+
+          <td align="center">
+            {{ $event->name }}
+          </td>
+
+          <td align="center">
+            {{ $event->host }}
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+
+          <td align="center">
+            {{ $event->price }}
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+
+          <td align="center">
+            <p>未実装</p>
+          </td>
+  @endforeach
 @endsection

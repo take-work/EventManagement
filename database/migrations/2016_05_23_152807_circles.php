7bbcb490e4b0e8b20c3fa28412634b3d338e0dcd
take-work <ts.lworkspace@gmail.com>
2016-05-23 15:32:11 +0900

    サークル情報を登録するためのcircleテーブルを追加した。(Issue #4)

---
+++ database/migrations/2016_05_23_152807_circles.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Circles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('circles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event_id');
+            $table->integer('number');
+            $table->string('space');
+            $table->string('circle_name');
+            $table->string('host');
+            $table->integer('staff');
+            $table->integer('desk');
+            $table->integer('chair');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('circles');
+    }
+}

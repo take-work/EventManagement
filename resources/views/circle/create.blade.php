17b7888e1368534a0d38b987e027e9b833bf36e6
take-work <ts.lworkspace@gmail.com>
2016-05-27 14:26:47 +0900

    サークルの新規登録フォームでバリデーションが働いた時、直前まで入力していた値が消えないようにした。(Issue #26)

--- resources/views/circle/create.blade.php
+++ resources/views/circle/create.blade.php
@@ -26,31 +26,31 @@
     <tbody>
       <tr>
         <td align="center">
-          <input name="number" type="text" id="number" />
+          <input name="number" type="text" id="number" value="{{ Input::old('number') }}" />
         </td>
 
         <td align="center">
-          <input name="space" type="text" id="space" />
+          <input name="space" type="text" id="space" value="{{ Input::old('space') }}" />
         </td>
 
         <td align="center">
-          <input name="circleName" type="text" id="circleName" maxlength="255" />
+          <input name="circleName" type="text" id="circleName" value="{{ Input::old('circleName') }}" />
         </td>
 
         <td align="center">
-          <input name="circleLeader" type="text" id="circleLeader" maxlength="255" />
+          <input name="circleLeader" type="text" id="circleLeader" value="{{ Input::old('circleLeader') }}" />
         </td>
 
         <td align="center">
-          <input name="staff" type="text" id="staff" maxlength="10" size="6" />人
+          <input name="staff" type="text" id="staff" maxlength="10" size="6" value="{{ Input::old('staff') }}" />人
         </td>
 
         <td align="center">
-          <input name="desk" type="text" id="desk" maxlength="10" size="6" />個
+          <input name="desk" type="text" id="desk" maxlength="10" size="6" value="{{ Input::old('desk') }}" />個
         </td>
 
         <td align="center">
-          <input name="chair" type="text" id="chair" maxlength="10" size="6" />個
+          <input name="chair" type="text" id="chair" maxlength="10" size="6" value="{{ Input::old('chair') }}" />個
         </td>
 
         <td align="center">

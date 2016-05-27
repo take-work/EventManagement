539bbcacaf1eb9c7476fde3067f79831a08e2108
take-work <ts.lworkspace@gmail.com>
2016-05-27 09:56:25 +0900

    金額情報の入力・更新ページで table を縦並びにした。(Issue #23)

--- resources/views/money/create.blade.php
+++ resources/views/money/create.blade.php
@@ -20,61 +20,63 @@
 
   <h3>金額情報入力</h3>
 
-  <table class="table table-responsive table-bordered">
-    <thead>
-      <tr class="active">
-        <th>100円玉</th>
-        <th>500円玉</th>
-        <th>1000円札</th>
-        <th>5000円札</th>
-        <th>10000円札</th>
-        <th>データの登録</th>
-      </tr>
-    </thead>
-
   {!! Form::open() !!}
-    <tbody>
+    <table class="table table-responsive table-bordered">
       <tr>
+        <th><div class="text-center">100円玉</div></th>
         <td align="center">
           <input type="button" value=" ー " onClick="minus(this.form.hundred)">
           <input name="hundred" type="text" id="hundred" size="6" align="center" value="0" />
           <input type="button" value=" ＋ " onClick="plus(this.form.hundred)">
         </td>
+        <th><div class="text-center">データの登録</div></th>
+      </tr>
 
+      <tr>
+        <th><div class="text-center">500円玉</div></th>
         <td align="center">
           <input type="button" value=" ー " onClick="minus(this.form.five_hundred)">
           <input name="five_hundred" type="text" id="five_hundred" size="6" align="center" value="0" />
           <input type="button" value=" ＋ " onClick="plus(this.form.five_hundred)">
         </td>
 
+        <td align="center" rowspan="4">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="id" value="{{ $id }}">
+          <input type="submit" value="登録する" style="width:100px; height:50px" />
+        </td>
+      </tr>
+
+      <tr>
+        <th><div class="text-center">1000円札</div></th>
         <td align="center">
           <input type="button" value=" ー " onClick="minus(this.form.thousand)">
           <input name="thousand" type="text" id="thousand" size="6" align="center" value="0" />
           <input type="button" value=" ＋ " onClick="plus(this.form.thousand)">
         </td>
+      </tr>
 
+      <tr>
+        <th><div class="text-center">5000円札</div></th>
         <td align="center">
           <input type="button" value=" ー " onClick="minus(this.form.five_thousand)">
           <input name="five_thousand" type="text" id="five_thousand" size="6" align="center" value="0" />
           <input type="button" value=" ＋ " onClick="plus(this.form.five_thousand)">
         </td>
+      </tr>
 
+      <tr>
+        <th><div class="text-center">10000円札</div></th>
         <td align="center">
           <input type="button" value=" ー " onClick="minus(this.form.million)">
           <input name="million" type="text" id="million" size="6" align="center" value="0" />
           <input type="button" value=" ＋ " onClick="plus(this.form.million)">
         </td>
+      </tr>
 
-        <td align="center">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <input type="hidden" name="id" value="{{ $id }}">
-          <input type="submit" value="登録する" />
-        </td>
-    </tr>
-  </tbody>
+    </table>
   {!! Form::close() !!}
 
-  </table>
 
   <hr>
   <a href="{!! url('/list') !!}">イベント一覧に戻る</a>

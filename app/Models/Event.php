3dd4cefe52072a1655afd5d4b90207ce1a4bf2e9
take-work <ts.lworkspace@gmail.com>
2016-05-30 10:01:27 +0900

    イベントの更新時に日付を入力したとき、数字を入力しただけで年月日の境界線に / を入れるようにした。(Issue #31)

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -26,19 +26,7 @@ class Event extends Model {
   public function insert($inputs) {
     $now = date("Y-m-d");
 
-    $start = $inputs['startDay'];
-    $end   = $inputs['endDay'];
-
-    $startYear  = mb_substr($start, 0, 4);
-    $startMonth = mb_substr($start, 4, 2);
-    $startDays  = mb_substr($start, 6, 2);
-
-    $endYear  = mb_substr($end, 0, 4);
-    $endMonth = mb_substr($end, 4, 2);
-    $endDays  = mb_substr($end, 6, 2);
-
-    $startDay = $startYear .'/'. $startMonth .'/'. $startDays;
-    $endDay   = $endYear .'/'. $endMonth .'/'. $endDays;
+    list ($startDay, $endDay) = $this->connectDate($inputs);
 
     DB::table('events')
       ->insert([
@@ -57,14 +45,16 @@ class Event extends Model {
   public function updateData($inputs) {
     $now = date("Y-m-d");
 
+    list ($startDay, $endDay) = $this->connectDate($inputs);
+
     DB::table('events')
       ->where('id', $inputs['id'])
       ->update([
         'name'       => $inputs['eventName'],
         'host'       => $inputs['host'],
         'price'      => $inputs['price'],
-        'startDay'   => $inputs['startDay'],
-        'endDay'     => $inputs['endDay'],
+        'startDay'   => $startDay,
+        'endDay'     => $endDay,
         'updated_at' => $now
       ]);
 
@@ -76,4 +66,23 @@ class Event extends Model {
 
     return true;
   }
+
+  public function connectDate($inputs) {
+    $start = $inputs['startDay'];
+    $end   = $inputs['endDay'];
+
+    $startYear  = mb_substr($start, 0, 4);
+    $startMonth = mb_substr($start, 4, 2);
+    $startDays  = mb_substr($start, 6, 2);
+
+    $endYear  = mb_substr($end, 0, 4);
+    $endMonth = mb_substr($end, 4, 2);
+    $endDays  = mb_substr($end, 6, 2);
+
+    $startDay = $startYear .'/'. $startMonth .'/'. $startDays;
+    $endDay   = $endYear .'/'. $endMonth .'/'. $endDays;
+
+    return array($startDay, $endDay);
+  }
+
 }

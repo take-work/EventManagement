2dc8b4ad02c720b8d537d6a6aa77cf07336a2704
take-work <ts.lworkspace@gmail.com>
2016-05-30 15:46:41 +0900

    スタッフ一覧のPDFファイルにイベント名・開始日・終了日のデータを追加した。(Issue #33)

--- app/Http/Controllers/staffPDFController.php
+++ app/Http/Controllers/staffPDFController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-//ZendPdfをインポート
 use ZendPdf\PdfDocument;
 use ZendPdf\Font;
 use ZendPdf\Page;
 
+use App\Models\Event;
+
 class staffPDFController extends Controller {
 
-  public function pdfCreate() {
+  public function pdfCreate($id) {
     $pdf = new PdfDocument();
 
     // 1ページ目のPDF
@@ -24,15 +25,25 @@ class staffPDFController extends Controller {
     $font = Font::fontWithPath('fonts/HanaMinA.ttf');
 
     // フォントと文字のサイズを指定
-    $firstPage->setFont($font , 24);
+    $firstPage->setFont($font , 18);
 
     // 出力する文字と位置、文字コードの指定
-    $firstPage->drawText("テスト", 100, 600, 'UTF-8');
+    $getEvent = $this->getEvent($id);
+    $firstPage->drawText($getEvent[0]->name, 150, 680, 'UTF-8');
+    $firstPage->drawText($getEvent[0]->startDay, 150, 655, 'UTF-8');
+    $firstPage->drawText($getEvent[0]->endDay, 369, 655, 'UTF-8');
 
     // ファイルとして保存、ブラウザに出力
     header ('Content-Type:', 'application/pdf');
     header ('Content-Disposition:', 'inline;');
     echo $pdf->render();
+  }
+
+  public function getEvent($id) {
+    $event = new Event();
+    $getEvent = $event->select($id);
+
+    return $getEvent;
   }
 
 }

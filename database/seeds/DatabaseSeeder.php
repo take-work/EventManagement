7231071a3802b2cf22ed1b3a08c5e53f2fe53ac6
take-work <ts.lworkspace@gmail.com>
2016-06-01 08:52:24 +0900

    staffs テーブルにテスト用のデータを50件作成した。(Issue #34)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,19 +3,43 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * データベース初期値設定実行
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
+use Illuminate\Support\Facades\DB;
 
-        // $this->call('UserTableSeeder');
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+use App\Models\Staff;
 
-        Model::reguard();
+class DatabaseSeeder extends Seeder {
+  /**
+   * データベース初期値設定実行
+   *
+   * @return void
+   */
+  public function run() {
+    Model::unguard();
+
+    $this->call('StaffsTableSeeder');
+
+    Model::reguard();
+  }
+}
+
+class StaffsTableSeeder extends Seeder {
+
+  public function run() {
+    $faker = Faker::create('en_US');  // ⑤
+
+    for ($i = 0; $i < 50; $i++) {  // ⑥
+      Staff::create([
+        'event_id'   => '1',
+        'name'       => '凛ちゃん',
+        'position'   => 'Hello,星を数えて',
+        'mail'       => 'Rinchan@example.com',
+        'tel'        => '000-1111-2222',
+        'twitter'    => 'rippi',
+        'experience' => '1',
+        'rank'       => '3'
+      ]);
     }
+  }
 }

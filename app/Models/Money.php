b880d6e3c351efdc49dd205b4373ed1e622673fa
take-work <ts.lworkspace@gmail.com>
2016-06-06 15:51:07 +0900

    金額管理のモデルファイルのインデントを修正してコメントを追加した。(Issue #41)

--- app/Models/Money.php
+++ app/Models/Money.php
@@ -14,63 +14,76 @@ class Money extends Model {
      */
     protected $table = 'money';
 
-  public function insert($inputs) {
-    $now = date("Y-m-d");
+    /*
+     * 金額情報の新規登録処理を行う関数
+     */
+    public function insert($inputs) {
+        $now = date("Y-m-d");
 
-    DB::table('money')->insert([
-      'event_id'      => $inputs['id'],
-      'hundred'       => $inputs['hundred'],
-      'five_hundred'  => $inputs['five_hundred'],
-      'thousand'      => $inputs['thousand'],
-      'five_thousand' => $inputs['five_thousand'],
-      'million'       => $inputs['million'],
-      'created_at'    => $now,
-      'updated_at'    => $now
-    ]);
+        DB::table('money')->insert([
+            'event_id'      => $inputs['id'],
+            'hundred'       => $inputs['hundred'],
+            'five_hundred'  => $inputs['five_hundred'],
+            'thousand'      => $inputs['thousand'],
+            'five_thousand' => $inputs['five_thousand'],
+            'million'       => $inputs['million'],
+            'created_at'    => $now,
+            'updated_at'    => $now
+        ]);
 
-    return true;
-  }
-
-  public function totalMpney($id) {
-    $money = DB::select('select * from money where event_id ='.$id);
-
-    if (!empty($money)) {
-      foreach ($money as $coin) {
-        $hundred       = $coin->hundred * 100;
-        $five_hundred  = $coin->five_hundred * 500;
-        $thousand      = $coin->thousand * 1000;
-        $five_thousand = $coin->five_thousand * 5000;
-        $million       = $coin->million * 10000;
-      }
-
-      $total = $hundred + $five_hundred + $thousand + $five_thousand + $million;
-    } else {
-      $total = null;
+        return true;
     }
 
-    return $total;
-  }
+    /*
+     * 金額情報の更新処理を行う関数
+     */
+    public function updateData($inputs) {
+        $now = date("Y-m-d");
 
-  public function updateData($inputs) {
-    $now = date("Y-m-d");
+        DB::table('money')
+            ->where('id', $inputs['id'])
+            ->update([
+                'hundred'       => $inputs['hundred'],
+                'five_hundred'  => $inputs['five_hundred'],
+                'thousand'      => $inputs['thousand'],
+                'five_thousand' => $inputs['five_thousand'],
+                'million'       => $inputs['million'],
+                'updated_at'    => $now
+            ]);
 
-    DB::table('money')
-      ->where('id', $inputs['id'])
-      ->update([
-        'hundred'       => $inputs['hundred'],
-        'five_hundred'  => $inputs['five_hundred'],
-        'thousand'      => $inputs['thousand'],
-        'five_thousand' => $inputs['five_thousand'],
-        'million'       => $inputs['million'],
-        'updated_at'    => $now
-      ]);
+        return true;
+    }
 
-    return true;
-  }
+    /*
+     * 登録された金額を計算する関数
+     */
+    public function totalMpney($id) {
+        $money = DB::select('select * from money where event_id ='. $id);
 
-  public function calculater($totalMoney = 0, $price = 0) {
-    $moneyCalc = $totalMoney - $price;
+        if (! empty($money)) {
+            foreach ($money as $coin) {
+                $hundred       = $coin->hundred * 100;
+                $five_hundred  = $coin->five_hundred * 500;
+                $thousand      = $coin->thousand * 1000;
+                $five_thousand = $coin->five_thousand * 5000;
+                $million       = $coin->million * 10000;
+            }
 
-    return $moneyCalc;
-  }
+            $total = $hundred + $five_hundred + $thousand + $five_thousand + $million;
+        } else {
+            $total = null;
+        }
+
+        return $total;
+    }
+
+    /*
+     * 合計金額 - 準備費用 を計算して純利益を出す関数
+     */
+    public function calculater($totalMoney = 0, $price = 0) {
+        $moneyCalc = $totalMoney - $price;
+
+        return $moneyCalc;
+    }
+
 }

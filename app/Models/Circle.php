62eab6a60acd6c7987579df0cd0ac94fd03990d1
take-work <ts.lworkspace@gmail.com>
2016-06-06 15:34:18 +0900

    サークル関連のモデルファイルのインデントを見直してコメントを追加し、delete 文が直書きだったのをクエリービルダーを使用するように書き換えた。(Issue #41)

--- app/Models/Circle.php
+++ app/Models/Circle.php
@@ -7,94 +7,119 @@ use DB;
 
 class Circle extends Model {
 
-  public function count($eventId) {
-    $counter = DB::table('circles')
-      ->select(DB::raw('count(*) as counter'))
-      ->where('event_id', $eventId)
-      ->get();
+    /**
+     * サークルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'circles';
 
-    return $counter;
-  }
+    /*
+     * イベント一覧ページでそのイベントに登録されているサークル数を出力するために数を数えている関数
+     */
+    public function count($eventId) {
+        $counter = DB::table('circles')
+            ->select(DB::raw('count(*) as counter'))
+            ->where('event_id', $eventId)
+            ->get();
 
-  public function deskCounter($id) {
-    $circles = Circle::where('event_id', $id)->get();
-
-    $first = true;
-    $desk = 0;
-
-    foreach ($circles as $circleData) {
-      if ($first == true) {
-        $desk = $circleData->desk;
-        $first = false;
-      } elseif($first == false) {
-        $desk += $circleData->desk;
-      }
+        return $counter;
     }
 
-    return $desk;
-  }
+    /*
+     * 必要な机の数を数えている関数
+     */
+    public function deskCounter($id) {
+        $circles = Circle::where('event_id', $id)->get();
 
-  public function chairCounter($id) {
-    $circles = Circle::where('event_id', $id)->get();
+        $first = true;
+        $desk = 0;
 
-    $first = true;
-    $chair = 0;
+        foreach ($circles as $circleData) {
+            if ($first == true) {
+                $desk = $circleData->desk;
+                $first = false;
+            } elseif($first == false) {
+                $desk += $circleData->desk;
+            }
+        }
 
-    foreach ($circles as $circleData) {
-      if ($first == true) {
-        $chair = $circleData->chair;
-        $first = false;
-      } else {
-        $chair += $circleData->chair;
-      }
+        return $desk;
     }
 
-    return $chair;
-  }
+    /*
+     * 必要な椅子の数を数えている関数
+     */
+    public function chairCounter($id) {
+        $circles = Circle::where('event_id', $id)->get();
 
-  public function insert($inputs) {
-    $now = date("Y-m-d");
+        $first = true;
+        $chair = 0;
 
-    DB::table('circles')->
-      insert([
-        'event_id'    => $inputs['id'],
-        'number'      => $inputs['number'],
-        'space'       => $inputs['space'],
-        'circle_name' => $inputs['circleName'],
-        'host'        => $inputs['circleLeader'],
-        'staff'       => $inputs['staff'],
-        'desk'        => $inputs['desk'],
-        'chair'       => $inputs['chair'],
-        'created_at'  => $now,
-        'updated_at'  => $now
-      ]);
+        foreach ($circles as $circleData) {
+            if ($first == true) {
+                $chair = $circleData->chair;
+                $first = false;
+            } else {
+                $chair += $circleData->chair;
+            }
+        }
 
-    return true;
-  }
+        return $chair;
+    }
 
-  public function updateData($inputs) {
-    $now = date("Y-m-d");
+    /*
+     * 新規登録処理を行う関数
+     */
+    public function insert($inputs) {
+        $now = date("Y-m-d");
 
-    DB::table('circles')
-      ->where('id', $inputs['id'])
-      ->update([
-        'number'      => $inputs['number'],
-        'space'       => $inputs['space'],
-        'circle_name' => $inputs['circleName'],
-        'host'        => $inputs['circleLeader'],
-        'staff'       => $inputs['staff'],
-        'desk'        => $inputs['desk'],
-        'chair'       => $inputs['chair'],
-        'updated_at'  => $now
-      ]);
+        DB::table('circles')->
+            insert([
+                'event_id'    => $inputs['id'],
+                'number'      => $inputs['number'],
+                'space'       => $inputs['space'],
+                'circle_name' => $inputs['circleName'],
+                'host'        => $inputs['circleLeader'],
+                'staff'       => $inputs['staff'],
+                'desk'        => $inputs['desk'],
+                'chair'       => $inputs['chair'],
+                'created_at'  => $now,
+                'updated_at'  => $now
+            ]);
 
-    return true;
-  }
+        return true;
+    }
 
-  public function deleteData($id) {
-    DB::delete('delete from circles where id = '. $id);
+    /*
+     * 更新処理を行う関数
+     */
+    public function updateData($inputs) {
+        $now = date("Y-m-d");
 
-    return true;
-  }
+        DB::table('circles')
+            ->where('id', $inputs['id'])
+            ->update([
+                'number'      => $inputs['number'],
+                'space'       => $inputs['space'],
+                'circle_name' => $inputs['circleName'],
+                'host'        => $inputs['circleLeader'],
+                'staff'       => $inputs['staff'],
+                'desk'        => $inputs['desk'],
+                'chair'       => $inputs['chair'],
+                'updated_at'  => $now
+            ]);
+
+        return true;
+    }
+
+    /*
+     * 削除処理を行う関数
+     */
+    public function deleteData($id) {
+        Circle::delete('id', $id);
+
+        return true;
+    }
 
 }

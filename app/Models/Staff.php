0b28fc6b6f7de5a09fdafe5876092af1fba7d569
take-work <ts.lworkspace@gmail.com>
2016-06-06 15:16:12 +0900

    スタッフ関連のモデルファイルでインデントの見直しとコメントの追加、その他SQL文をクエリービルダーに書き換えを行った。(Issue #41)

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,69 +3,82 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query;
 use DB;
 
 class Staff extends Model {
-  /**
-   * スタッフと関連しているテーブル
-   *
-   * @var string
-   */
-  protected $table = 'staffs';
 
-  public function count($eventId) {
-    $counter = DB::table('staffs')
-                 ->select(DB::raw('count(*) as counter'))
-                 ->where('event_id', $eventId)
-                 ->get();
+    /**
+     * スタッフと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'staffs';
 
-    return $counter;
-  }
+    /*
+     * イベント一覧ページで、そのイベントに登録されているスタッフの総数を出力するための関数
+     */
+    public function count($eventId) {
+        $counter =
+            DB::table('staffs')
+                ->select(DB::raw('count(*) as counter'))
+                ->where('event_id', $eventId)
+                ->get();
 
-  public function insert($inputs) {
-    $now = date("Y-m-d");
+        return $counter;
+    }
 
-    DB::table('staffs')
-      ->insert([
-        'event_id'   => $inputs['id'],
-        'name'       => $inputs['staffName'],
-        'position'   => $inputs['position'],
-        'mail'       => $inputs['mail'],
-        'tel'        => $inputs['tel'],
-        'twitter'    => $inputs['twitter'],
-        'experience' => $inputs['experience'],
-        'rank'       => $inputs['rank'],
-        'updated_at' => $now,
-        'created_at' => $now
-      ]);
+    /*
+     * スタッフの新規登録処理を行う関数
+     */
+    public function insert($inputs) {
+        $now = date("Y-m-d");
 
-  return true;
-  }
+        DB::table('staffs')
+            ->insert([
+                'event_id'   => $inputs['id'],
+                'name'       => $inputs['staffName'],
+                'position'   => $inputs['position'],
+                'mail'       => $inputs['mail'],
+                'tel'        => $inputs['tel'],
+                'twitter'    => $inputs['twitter'],
+                'experience' => $inputs['experience'],
+                'rank'       => $inputs['rank'],
+                'updated_at' => $now,
+                'created_at' => $now
+        ]);
 
-  public function updateData($inputs) {
-    $now = date("Y-m-d");
+        return true;
+    }
 
-    DB::table('staffs')
-      ->where('id', $inputs['id'])
-      ->update([
-        'name'       => $inputs['staffName'],
-        'position'   => $inputs['position'],
-        'mail'       => $inputs['mail'],
-        'tel'        => $inputs['tel'],
-        'twitter'    => $inputs['twitter'],
-        'experience' => $inputs['experience'],
-        'rank'       => $inputs['rank'],
-        'updated_at' => $now
-      ]);
+    /*
+     * スタッフ情報の更新処理を行う関数
+     */
+    public function updateData($inputs) {
+        $now = date("Y-m-d");
 
-    return true;
-  }
+        DB::table('staffs')
+            ->where('id', $inputs['id'])
+            ->update([
+                'name'       => $inputs['staffName'],
+                'position'   => $inputs['position'],
+                'mail'       => $inputs['mail'],
+                'tel'        => $inputs['tel'],
+                'twitter'    => $inputs['twitter'],
+                'experience' => $inputs['experience'],
+                'rank'       => $inputs['rank'],
+                'updated_at' => $now
+            ]);
 
-  public function deleteData($id) {
-    DB::delete('delete from staffs where id = '. $id);
+        return true;
+    }
 
-    return true;
-  }
+    /*
+     * スタッフ情報の削除処理を行う関数
+     */
+    public function deleteData($id) {
+        Staff::delete('id', $id);
+
+        return true;
+    }
 
 }

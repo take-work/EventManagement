979b7f6971fdadcbb9d62575239bf9d7ebc6a1cc
take-work <ts.lworkspace@gmail.com>
2016-06-15 12:55:33 +0900

    イベント一覧ページで開始日以外でも検索できるようにした。(Issue #42)

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -36,14 +36,13 @@ class Event extends Model {
      * events テーブルから検索されたときに検索結果を返す関数
      */
     public function search($inputs) {
-        $searchQuery = Event::query();
         $searchContent = $inputs['searchContents'];
         $searchText    = $inputs['searchText'];
 
-        if ($searchContent === "startDay") {
-            $searchQuery->where('startDay', 'like', '%'. $searchText .'%');
-            $events = $searchQuery->paginate(10);
-        }
+        $searchQuery = Event::query();
+        $searchQuery->where($searchContent, 'like', '%'. $searchText .'%');
+
+        $events = $searchQuery->paginate(10);
 
         return $events;
     }

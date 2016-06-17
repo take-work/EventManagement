40c9849b40fd1598bea29ccb3a1e6315c4b032d7
take-work <ts.lworkspace@gmail.com>
2016-06-17 15:34:06 +0900

    イベント一覧の検索フォームに tbody 属性を追加した。(Issue #45)

--- resources/views/event/list.blade.php
+++ resources/views/event/list.blade.php
@@ -25,23 +25,25 @@
         </tr>
       </thead>
 
-      <tr>
-        <td align="center">
-          <input type="radio" name="searchContents" value="startDay" id="startDay"><label for="startDay">開始日</label> &nbsp;
-          <input type="radio" name="searchContents" value="endDay" id="endDay"><label for="endDay">終了日</label> &nbsp;
-          <input type="radio" name="searchContents" value="name" id="name"><label for="name">イベント名</label> &nbsp;
-          <input type="radio" name="searchContents" value="host" id="host"><label for="host">主催者</label>
-        </td>
-        <td align="center">
-          <input type="text" name="searchText">
-        </td>
-        <td align="center">
-          <input type="submit" value="検索する">
-        </td>
-        <td align="center">
-          <a href="{!! url('list') !!}"><input type="button" value="検索をリセットする"></a>
-        </td>
-      </tr>
+      <tbody>
+        <tr>
+          <td align="center">
+            <input type="radio" name="searchContents" value="startDay" id="startDay"><label for="startDay">開始日</label> &nbsp;
+            <input type="radio" name="searchContents" value="endDay" id="endDay"><label for="endDay">終了日</label> &nbsp;
+            <input type="radio" name="searchContents" value="name" id="name"><label for="name">イベント名</label> &nbsp;
+            <input type="radio" name="searchContents" value="host" id="host"><label for="host">主催者</label>
+          </td>
+          <td align="center">
+            <input type="text" name="searchText">
+          </td>
+          <td align="center">
+            <input type="submit" value="検索する">
+          </td>
+          <td align="center">
+            <a href="{!! url('list') !!}"><input type="button" value="検索をリセットする"></a>
+          </td>
+        </tr>
+      </tbody>
     </table>
   {!! Form::close() !!}
 

2e9306cce7401045ad66f0f84fa0824dfc6b2a1d
take-work <ts.lworkspace@gmail.com>
2016-06-20 10:48:44 +0900

    StaffPDFController のインデントを修正した。(Issue #33, #41)

--- app/Http/Controllers/StaffPDFController.php
+++ app/Http/Controllers/StaffPDFController.php
@@ -17,81 +17,82 @@ use App\Models\Event;
 
 class StaffPDFController extends Controller {
 
-  public function pdfCreate($id) {
-    $pdfDocument = new PdfDocument();
-    $extractor = new Extractor();
+    public function pdfCreate($id) {
+        $pdfDocument = new PdfDocument();
+        $extractor = new Extractor();
 
-    // 1ページ目のPDF
-    $files = ['PDF/staffList.pdf', 'PDF/staffListTwo.pdf'];
+        // 1ページ目のPDF
+        $files = ['PDF/staffList.pdf', 'PDF/staffListTwo.pdf'];
 
-    foreach ($files as $file) {
-      $pdf = PdfDocument::load($file);
+        foreach ($files as $file) {
+            $pdf = PdfDocument::load($file);
 
-      foreach ($pdf->pages as $page) {
-        $pdfExtract = $extractor->clonePage($page);
-        $pdfDocument->pages[] = $pdfExtract;
-      }
-    }
-    $firstPage = $pdfDocument->pages[0];
-    $secondPage = $pdfDocument->pages[1];
+            foreach ($pdf->pages as $page) {
+                $pdfExtract = $extractor->clonePage($page);
+                $pdfDocument->pages[] = $pdfExtract;
+            }
+        }
 
-    $font = Font::fontWithPath('fonts/HanaMinA.ttf');
+        $firstPage = $pdfDocument->pages[0];
+        $secondPage = $pdfDocument->pages[1];
 
-    $firstPage->setFont($font , 18);
-    $secondPage->setFont($font, 18);
+        $font = Font::fontWithPath('fonts/HanaMinA.ttf');
 
-    // 出力する文字と位置、文字コードの指定
-    $getEvent = $this->getEvent($id);
-    $firstPage->drawText($getEvent[0]->name, 150, 458, 'UTF-8');
-    $firstPage->drawText($getEvent[0]->startDay, 150, 432, 'UTF-8');
-    $firstPage->drawText($getEvent[0]->endDay, 415, 432, 'UTF-8');
+        $firstPage->setFont($font , 18);
+        $secondPage->setFont($font, 18);
 
-    $staffs = $this->getStaffs($id);
+        // 出力する文字と位置、文字コードの指定
+        $getEvent = $this->getEvent($id);
+        $firstPage->drawText($getEvent[0]->name, 150, 458, 'UTF-8');
+        $firstPage->drawText($getEvent[0]->startDay, 150, 432, 'UTF-8');
+        $firstPage->drawText($getEvent[0]->endDay, 415, 432, 'UTF-8');
 
-    $y = 345;
-    $staffCount = 0;
+        $staffs = $this->getStaffs($id);
 
-    foreach ($staffs as $staff) {
-      $firstPage->setFont($font , 12);
-
-      $firstPage->drawText($staff->name, 80, $y, 'UTF-8');
-      $firstPage->drawText($staff->position, 215, $y, 'UTF-8');
-
-      $firstPage->setFont($font , 8);
-
-      $firstPage->drawText($staff->mail, 370, $y, 'UTF-8');
-      $firstPage->drawText($staff->tel, 535, $y, 'UTF-8');
-      $firstPage->drawText($staff->twitter, 620, $y, 'UTF-8');
-
-      $y = $y - 28;
-      $staffCount++;
-
-      if ($staffCount == 11) {
         $y = 345;
-      }
+        $staffCount = 0;
 
-      if ($staffCount >= 11) {
-        $secondPage->drawText($staff->name, 80, $y, 'UTF-8');
-      }
+        foreach ($staffs as $staff) {
+            $firstPage->setFont($font , 12);
+
+            $firstPage->drawText($staff->name, 80, $y, 'UTF-8');
+            $firstPage->drawText($staff->position, 215, $y, 'UTF-8');
+
+            $firstPage->setFont($font , 8);
+
+            $firstPage->drawText($staff->mail, 370, $y, 'UTF-8');
+            $firstPage->drawText($staff->tel, 535, $y, 'UTF-8');
+            $firstPage->drawText($staff->twitter, 620, $y, 'UTF-8');
+
+            $y = $y - 28;
+            $staffCount++;
+
+            if ($staffCount == 11) {
+                $y = 345;
+            }
+
+            if ($staffCount >= 11) {
+                $secondPage->drawText($staff->name, 80, $y, 'UTF-8');
+            }
+        }
+
+        // ファイルとして保存、ブラウザに出力
+        header ('Content-Type:', 'application/pdf');
+        header ('Content-Disposition:', 'inline;');
+        echo $pdfDocument->render();
     }
 
-    // ファイルとして保存、ブラウザに出力
-    header ('Content-Type:', 'application/pdf');
-    header ('Content-Disposition:', 'inline;');
-    echo $pdfDocument->render();
-  }
+    public function getEvent($id) {
+        $event = new Event();
+        $getEvent = $event->select($id);
 
-  public function getEvent($id) {
-    $event = new Event();
-    $getEvent = $event->select($id);
+        return $getEvent;
+    }
 
-    return $getEvent;
-  }
+    public function getStaffs($id) {
+        $staffs = DB::select('select * from staffs where event_id = '. $id);
 
-  public function getStaffs($id) {
-    $staffs = DB::select('select * from staffs where event_id = '. $id);
-
-    return $staffs;
-  }
+        return $staffs;
+    }
 
 }

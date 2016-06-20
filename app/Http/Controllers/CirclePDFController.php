066dfe106025474f1d43ba78bac54c327e4f9f34
take-work <ts.lworkspace@gmail.com>
2016-06-20 16:55:54 +0900

    サークルのPDF作成ファイルのインデントを修正した。(Issue #41)

--- app/Http/Controllers/CirclePDFController.php
+++ app/Http/Controllers/CirclePDFController.php
@@ -16,43 +16,43 @@ use App\Models\Event;
 
 class CirclePDFController extends Controller {
 
-  public function pdfCreate($id) {
-    $pdf = new PdfDocument();
+    public function pdfCreate($id) {
+        $pdf = new PdfDocument();
 
-    // 1ページ目のPDF
-    $pdf = PdfDocument::load('PDF/circleList.pdf');
-    $firstPage = $pdf->pages[0];
+        // 1ページ目のPDF
+        $pdf = PdfDocument::load('PDF/circleList.pdf');
+        $firstPage = $pdf->pages[0];
 
-    $font = Font::fontWithPath('fonts/HanaMinA.ttf');
+        $font = Font::fontWithPath('fonts/HanaMinA.ttf');
 
-    // フォントと文字のサイズを指定
-    $firstPage->setFont($font , 18);
+        // フォントと文字のサイズを指定
+        $firstPage->setFont($font , 18);
 
-    // 出力する文字と位置、文字コードの指定
-    $getEvent = $this->getEvent($id);
-    $firstPage->drawText($getEvent[0]->name, 128, 717, 'UTF-8');
-    $firstPage->drawText($getEvent[0]->startDay, 128, 692, 'UTF-8');
-    $firstPage->drawText($getEvent[0]->endDay, 347, 692, 'UTF-8');
+        // 出力する文字と位置、文字コードの指定
+        $getEvent = $this->getEvent($id);
+        $firstPage->drawText($getEvent[0]->name, 128, 717, 'UTF-8');
+        $firstPage->drawText($getEvent[0]->startDay, 128, 692, 'UTF-8');
+        $firstPage->drawText($getEvent[0]->endDay, 347, 692, 'UTF-8');
 
-    $circles = $this->getCircles($id);
+        $circles = $this->getCircles($id);
 
-    // ファイルとして保存、ブラウザに出力
-    header ('Content-Type:', 'application/pdf');
-    header ('Content-Disposition:', 'inline;');
-    echo $pdf->render();
-  }
+        // ファイルとして保存、ブラウザに出力
+        header ('Content-Type:', 'application/pdf');
+        header ('Content-Disposition:', 'inline;');
+        echo $pdf->render();
+    }
 
-  public function getEvent($id) {
-    $event = new Event();
-    $getEvent = $event->select($id);
+    public function getEvent($id) {
+        $event = new Event();
+        $getEvent = $event->select($id);
 
-    return $getEvent;
-  }
+        return $getEvent;
+    }
 
-  public function getCircles($id) {
-    $circles = DB::select('select * from circles where event_id = '. $id);
+    public function getCircles($id) {
+        $circles = DB::select('select * from circles where event_id = '. $id);
 
-    return $circles;
-  }
+        return $circles;
+    }
 
 }

7897a1f2d6c72b7993dde1d9cea99411d59b1978
take-work <ts.lworkspace@gmail.com>
2016-06-22 18:03:29 +0900

    ログインしている状態で /list にアクセスするとイベント一覧ページに行くことをテストした。(Issue #52)

--- tests/Event/EventTest.php
+++ tests/Event/EventTest.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\User;
+
 class EventTest extends TestCase {
-    /**
+    /*
+     * 最初にログインする。
+     */
+    public function setUp() {
+        parent::setUp();
+
+        $user = new User(['user' => 'take']);
+        $this->be($user);
+    }
+
+	/**
      * /list にアクセスするとイベント一覧ページが開く。
      *
      * @return void
      */
     public function testBasicExample() {
-        $this->visit('/')
-            ->see('ログイン画面');
+        $this->visit('/list')
+            ->see('イベント一覧');
     }
 }

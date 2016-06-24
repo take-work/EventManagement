036991ed41b3557e398a97190dc39fb554d8d712
take-work <ts.lworkspace@gmail.com>
2016-06-24 10:27:24 +0900

    スタッフ情報を正常に登録できることをテストした。(Issue #52)

--- tests/Staff/StaffTest.php
+++ tests/Staff/StaffTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use DB;
 
 class StaffTest extends TestCase {
     /*
@@ -17,8 +18,41 @@ class StaffTest extends TestCase {
      * /staffList にアクセスするとスタッフ一覧ページが開く。
      */
     public function testStaffListAccess() {
+        $query = DB::table('events')->get();
+        $id = $query[0]->id;
+
         $this
-            ->visit('/staffList/8')
+            ->visit('/staffList/'.$id)
             ->see('スタッフ一覧');
     }
+
+    /*
+     * スタッフを新規登録できる。
+     */
+    public function testStaffCreate() {
+        // スタッフ名を生成する。
+        $faker = Faker\Factory::create('ja_JP');
+        $staffName = $faker->unique()->name;
+
+        $query = DB::table('events')->get();
+        $id = $query[0]->id;
+
+        $this
+            ->visit('/staffCreate/'.$id)
+            ->see('スタッフ情報入力')
+            ->type($staffName, 'staffName')
+            ->type('担当/持ち場', 'position')
+            ->type('aaa@example.co.jp', 'mail')
+            ->type('000-0000-0000', 'tel')
+            ->type('twitter', 'twitter')
+            ->select('1', 'experience')
+            ->select('1', 'rank')
+            ->press('登録する')
+            ->see('新スタッフ「'.$staffName.'」さんを新規登録しました。');
+
+        // faker で生成されたスタッフ名が正しくデータベースに登録されているかチェックする。
+        $this
+            ->seeInDatabase('staffs', ['name' => $staffName]);
+    }
+
 }
